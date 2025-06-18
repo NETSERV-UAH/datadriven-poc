be36a2013bfe31a5db04cca10fec2de9b649f423
Miguel Nicolas Manso Nuñez <miguel.manso@uah.es>
2025-06-18 12:41:58 +0200

    [V0.1.2] Grapaha directory fixing

--- kubernettes/containers/Migrados/grafana/entrypoint.sh
+++ kubernettes/containers/Migrados/grafana/entrypoint.sh
@@ -6,17 +6,11 @@ echo "[INFO] Iniciando contenedor de Grafana..."
 # Descargamos los ficheros de configuracion
 echo "[INFO] Descargando ficheros"
 
-mkdir /etc/grafana/dashboards
-wget -q -O /etc/grafana/dashboards/dashboard.yml https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/containers/Migrados/grafana/provisioning/dashboards/dashboards.yaml
+wget -q -O /var/lib/grafana/dashboards/dashboard.json https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/containers/Migrados/grafana/dashboards/dashboard.json
 
-# mkdir /etc/grafana/provisioning/
+wget -q -O /etc/grafana/provisioning/dashboards/dashboard.yaml https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/containers/Migrados/grafana/provisioning/dashboards/dashboards.yaml
 
-# mkdir /etc/grafana/provisioning/dashboards/
-wget -q -O /etc/grafana/provisioning/dashboards/dashboard.json https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/containers/Migrados/grafana/dashboards/dashboard.json
-
-# mkdir /etc/grafana/provisioning/datasources/
 wget -q -O /etc/grafana/provisioning/datasources/influxdb_datasource.yaml https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/containers/Migrados/grafana/provisioning/datasources/influxdb_datasource.yaml
 
-# Ejecutar Grafana como proceso principal del contenedor
 echo "[INFO] Lanzando grafana..."
 exec /usr/sbin/grafana-server --config=/etc/grafana/grafana.ini --homepath=/usr/share/grafana web
