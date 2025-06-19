98f4cd1cdabb4e6ff7fe4e842da198feafc792d2
Miguel Nicolas Manso Nuñez <miguel.manso@uah.es>
2025-06-19 12:19:51 +0200

    [V0.1.7] VMs manage and basic attempts

--- kubernettes/containers/Pendientes/influxdb/config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-########################
-# Config  InfluxDB     #
-########################
-
-curl http://localhost:8086/api/v2/setup --data '{"username": "jorge", "password": "ELPORRAS", "token": "3hv3m8nphSlHRbVbKQ7o5Hrm0S4FhLDhu8WWGt9abXHQ26Ked4hGDSRqtZsYC-hc2gS9snCLjN5p9OnoYBeRYA==", "bucket": "iiot_data", "org": "UAH"}'
