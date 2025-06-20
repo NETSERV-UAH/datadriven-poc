f1c95a9ba3b89e3d7066fb0611a0bfa5273712e3
Miguel Nicolas Manso Nuñez <miguel.manso@uah.es>
2025-06-20 18:29:23 +0200

    [V0.1.6] Fixing

--- kubernettes/vagrant-k8s/scripts/kubevirt.sh
+++ kubernettes/vagrant-k8s/scripts/kubevirt.sh
@@ -4,5 +4,8 @@ sudo -u vagrant -i bash <<EOF
 export KUBEVIRT_VERSION=\$(curl -s https://api.github.com/repos/kubevirt/kubevirt/releases/latest | grep tag_name | cut -d '"' -f 4)
 kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/\${KUBEVIRT_VERSION}/kubevirt-operator.yaml
 kubectl create -f https://github.com/kubevirt/kubevirt/releases/download/\${KUBEVIRT_VERSION}/kubevirt-cr.yaml
+kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/latest/download/cdi-operator.yaml
+kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/latest/download/cdi-cr.yaml
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 EOF
 
--- kubernettes/vagrant-k8s/scripts/project.sh
+++ kubernettes/vagrant-k8s/scripts/project.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 sudo -u vagrant -i bash <<'EOF'
+# Datadriven
+# ns
+kubectl apply -f https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/k8s/datadriven/datadriven-ns.yaml
+# influxdb
+kubectl apply -f https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/k8s/datadriven/influxdb/influxdb-secrets.yaml
+kubectl apply -f https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/k8s/datadriven/influxdb/influxdb-pvc.yaml
+kubectl apply -f https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/k8s/datadriven/influxdb/influxdb-deployment.yaml
+kubectl apply -f https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/k8s/datadriven/influxdb/influxdb-service.yaml
+# Grafana
+kubectl apply -f https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/k8s/datadriven/grafana/grafana-secrets.yaml
+kubectl apply -f https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/k8s/datadriven/grafana/grafana-pvc.yaml
+kubectl apply -f https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/k8s/datadriven/grafana/grafana-deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/NETSERV-UAH/datadriven-poc/refs/heads/k8s-deploy-datadriven/kubernettes/k8s/datadriven/grafana/grafana-service.yaml
+
+# factory-floor
 
 
 EOF
